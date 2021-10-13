c7be146c324764d076494976738c03743116f034
dariussandru <91565295+dariussandru@users.noreply.github.com>
2021-10-13 10:01:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -97,30 +97,31 @@ def test_nr_divizori():
 
 def get_longest_same_div_count(lista_principala):
     """
-    Functia determina cea mai lunga secventa co prop. ca numerele au acelas nr de divizori
-    :param lista_principala: toata lista de numere
-    :return: secventa cea mai lunga cu proprietatea data
+    Functie care determina cea mai lunga subsecventa in care toate numerele au acelasi numar de divizori
+    :param lst:
+    :return: O lista de intregi care reprezinta cea mai lunga subsecventa in care toate numerele au acelasi numar de divizori
     """
-
-    lungime_max = 0
+    
+    lmax = 0
     secv_max = []
-    lungime_actuala = 1
+    lactual = 1
+    nrdiv = nr_divizori(lista_principala[0])
     secv_actuala = [lista_principala[0]]
-    nrdivizori = nr_divizori(lista_principala[0])
-
-    for n in range(1, len(lista_principala)):
-        if nr_divizori(lista_principala[n]) == nrdivizori:
-            lungime_actuala += 1
+    
+    for n in range (1,len(lista_principala)):
+       if(nr_divizori(lista_principala[n]) == nrdiv):
+            lactual = lactual + 1
             secv_actuala.append(lista_principala[n])
-        else:
-            if lungime_actuala > lungime_max:
-                lungime_max = lungime_actuala
+       else:
+           if(lactual > lmax):
+                lmax = lactual
                 secv_max = secv_actuala
-
-            secv_actuala = lista_principala[n]
-            lungime_actuala = 1
-            nrdivizori = nr_divizori(lista_principala[n])
-
+           secv_actuala = [lista_principala[n]]
+           lactual = 1
+           nrdiv = nr_divizori(lista_principala[n])
+            
+    if(lactual > lmax):
+        secv_max = secv_actuala
     return secv_max
 
 
@@ -135,18 +136,136 @@ def test_get_longest_same_div_count():
     assert (get_longest_same_div_count([6, 9, 25, 49, 10, 8, 2]) == [9, 25, 49])
 
 
+def is_palindrome(n):
+    """
+    :param copie: retinem nr n
+    :param invers: formam inversul numarului n
+    :param n: numarul pe care il verificam daca este palindrom
+    :return: true daca nr este palindrom sau false in caz contrar
+    """
+    copie = n
+    invers = 0
+    while copie > 0:
+        invers = invers * 10 + copie % 10
+        copie = copie // 10
+    if n == invers:
+        return True
+    else:
+        return False
+
+def test_nr_palindrome():
+    assert is_palindrome(121) == True
+    assert is_palindrome(123) == False
+    assert is_palindrome(232) == True
+
+
+def get_longest_all_palindromes(lista_principala):
+    """
+
+    :param lista_principala:
+    :return: cea mai lunga subsecventa de nr palindrome
+    """
+    lungime_max = 0
+    lungime_actuala = 0
+    secventa_max = []
+    secventa_actuala = []
+    for n in lista_principala:
+        if is_palindrome(n):
+            lungime_actuala +=  1
+            secventa_actuala.append(n)
+        else:
+            if lungime_actuala > lungime_max:
+                lungime_max = lungime_actuala
+                secventa_max = secventa_actuala
+
+            lungime_actuala = 0
+            secventa_actuala = []
+
+    if lungime_actuala > lungime_max:
+        secventa_max = secventa_actuala
+
+    return secventa_max
+
+def test_get_longest_all_palindromes():
+    assert get_longest_all_palindromes([121, 232, 343]) == [121, 232, 343]
+    assert get_longest_all_palindromes([21, 212, 121]) == [212 ,121]
+
+
+def is_crescator(x):
+    """
+    
+    :param x: un numar natural
+    :return: true daca cifrele numarului x sunt in ordine crescatoare si fals in caz contrar
+    """
+    copie = x
+    nr_cifre = 0
+    var = 0
+    while copie > 9:
+        nr_cifre += 1
+        copie //= 10
+    copie = x
+
+    for i in range(copie//10**nr_cifre , copie//10%10+1):
+        for j in range(copie//10**(nr_cifre-1)%10, copie%10+1):
+            if(i<j):
+                var+=1
+    if var >= nr_cifre:
+        return True
+    else:
+        return False
+
+def test_is_crescator():
+    assert is_crescator(123) == True
+    assert is_crescator(12343) == False
+    assert is_crescator(321) ==False
+
+
+def get_longest_digit_count_desc(lista_principala):
+    """
+
+       :param lista_principala:
+       :return: cea mai lunga subsecventa de numere cu prop ca cifrele numerelor sunt in oridine crescatoare
+       """
+    lungime_max = 0
+    lungime_actuala = 0
+    secventa_max = []
+    secventa_actuala = []
+    for x in lista_principala:
+        if is_crescator(x):
+            lungime_actuala += 1
+            secventa_actuala.append(x)
+        else:
+            if lungime_actuala > lungime_max:
+                lungime_max = lungime_actuala
+                secventa_max = secventa_actuala
+
+            lungime_actuala = 0
+            secventa_actuala = []
+
+    if lungime_actuala > lungime_max:
+        secventa_max = secventa_actuala
+
+    return secventa_max
+
+
 def main():
     test_nr_prim()
     test_get_longest_all_primes()
     test_nr_divizori()
-    test_get_longest_all_primes()
+    test_get_longest_same_div_count()
+    test_nr_palindrome()
+    test_get_longest_all_palindromes()
+    test_is_crescator()
+
     lista_principala = []
     f = False
     while f != True:
         print("1. Citire data pentru lista")
         print("2. Det. cea mai lunga subsecventa cu proprietatea 1")
         print("3. Det. cea mai lunga subsecventa cu proprietatea 2")
-        print("4. Iesire din program")
+        print("4. Det. cea mai lunga subsecventa cu proprietatea 3")
+        print("5. Det. cea mai lunga subsecventa cu proprietatea 4")
+        print("6. Iesire din program")
         print()
         optiune = int(input("Dati obtiunea:"))
 
@@ -162,5 +281,12 @@ def main():
             print(secv2)
             print()
         if optiune == 4:
+            secv3 = get_longest_all_palindromes(lista_principala)
+            print(secv3)
+            print()
+        if optiune == 5 :
+            secv4 = get_longest_digit_count_desc(lista_principala)
+            print(secv4)
+        if optiune == 6:
             f = True
 main()
